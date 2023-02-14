c10e36997bd993da3ee81e63843756278ac9dd70
kskarthik5 <88155658+kskarthik5@users.noreply.github.com>
2023-02-14 11:17:51 +0530

    done

--- SPCC/firstfollow.py
+++ SPCC/firstfollow.py
@@ -1,8 +1,10 @@
 grammar={"S":["Aa","Ba"],
-        "A":["Ba"],
-        "B":["c"]
+        "A":["aB"],
+        "B":["c"],
 }
 def getFirst(sym):
+    if( sym not in grammar.keys()):
+        return sym
     s=set()
     def first(sym):
         vals=grammar[sym]
@@ -16,12 +18,26 @@ def getFirst(sym):
             first(e[0])
         else:
             s.add(e[0])
-    print(s)
+    return s
         
-def getFollow(sym):
+def getFollow(val):
     s=set()
+    def follow(sym):
+        for e in grammar.keys():
+            for i in grammar[e]:
+                if sym in i:
+                    #rules
+                    if(i[-1]==sym):
+                        follow(e)
+                    else:
+                        new=i[i.index(sym)+1]
+                        if(new >="A" and new<="Z"):
+                            follow(new)
+                        else:
+                            s.add(getFirst(new))
+    follow(val)
+    return s
     
-
 i=0
 # while(True):
 #     args=input("Enter Production Rule (Enter - to stop) > ").split()
@@ -29,9 +45,14 @@ i=0
 #         break
 #     grammar[args[0]]=args[1:]
 # print(grammar)
+
 while(True):
-    args=input("First(0),Follow(1) : <val> > ").split()
+    args=input("First(0)/Follow(1) : <val> > ").split()
+    if(len(args)==0):
+        break
+    res=None
     if(args[0]=='0'):
-        getFirst(args[1])
+        res=getFirst(args[1])
     if(args[0]=='1'):
-        getFollow(args[1])
+        res=getFollow(args[1])
+    print(*res)
